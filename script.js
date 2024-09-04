11643f08d021089d24d28074456eb45515860766
DGStarleon <dgstarleon.projectid@gmail.com>
2024-09-05 02:30:22 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function changeHeroTextColor() {
 
 setInterval(changeHeroTextColor, 2000);
 let currentImageIndex = 0;
-const images = ["img/logo1.jpeg", "img/produk1.jpeg", "img/produk2.jpeg"];
+const images = ["https://scontent.fcgk32-1.fna.fbcdn.net/v/t39.30808-6/455424988_122171239292195955_3156477280960972887_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGHm_pv1KBaJytsQm8rDg9iAHpo_0FG7VwAemj_QUbtXGsPVc0pIwWsUSNm82NkH0T69iro3_4uJ0nSmkhgorMt&_nc_ohc=ryZPwMFUciMQ7kNvgH_U0h7&_nc_ht=scontent.fcgk32-1.fna&oh=00_AYC8gDgF9Zku0UsgzLOkl7Gd_zUGHLJ0Vh1Yq78BUH0T6A&oe=66DE2BB5", "https://scontent.fcgk32-1.fna.fbcdn.net/v/t39.30808-6/455746074_122171239376195955_7549034585695606824_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFpoYlWsphDECMzPzWz3lAy3iCPDxRrU8XeII8PFGtTxTlNHj2DEpUlR2ex5bGzIY71aqYpzoU3rdroP9AWk9gL&_nc_ohc=rFoKqzDcjjcQ7kNvgG2nV8l&_nc_ht=scontent.fcgk32-1.fna&oh=00_AYBJy5K0QVaTEp8lhaQc_hz2NmoLqb69APboSHujvU4zFA&oe=66DE3555https://scontent.fcgk32-1.fna.fbcdn.net/v/t39.30808-6/455746074_122171239376195955_7549034585695606824_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFpoYlWsphDECMzPzWz3lAy3iCPDxRrU8XeII8PFGtTxTlNHj2DEpUlR2ex5bGzIY71aqYpzoU3rdroP9AWk9gL&_nc_ohc=rFoKqzDcjjcQ7kNvgG2nV8l&_nc_ht=scontent.fcgk32-1.fna&oh=00_AYBJy5K0QVaTEp8lhaQc_hz2NmoLqb69APboSHujvU4zFA&oe=66DE3555"];
 
 function changeHeroImage() {
     currentImageIndex = (currentImageIndex + 1) % images.length;
